6a8887d6f188599f4c0ab77d79f791dacd954ee1
SulaimanAlfarsi <sulaimanalfarsi3060@gmail.com>
2026-02-17 10:27:29 +0400

    Inital calculator setup

---
+++ src/calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+p
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+
+    }
+
+
+
+}
